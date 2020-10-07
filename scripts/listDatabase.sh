1c427968643afb2645f0252ffee3e08d428a9ac9
Daniel Steudler <daniel.steudler@gmx.ch>
2020-10-07 13:45:44 +0200

    simplified request to database. Use jq for json creation and parsing
    * Database will now only be queried if you start typing.
    * Only matching entries will be listed
    * Entry name is without the path is the title
    * Entry name with the path is the subtitle

--- scripts/listDatabase.sh
+++ scripts/listDatabase.sh
@@ -4,26 +4,24 @@ export PATH='/usr/local/bin/:/usr/bin'
 
 if [[ -z ${database} ]] || [[ -z ${keychain} ]];
 then
-    
+
     echo "{\"items\": [{\"title\":\"Not configured, please run: kpassinit\"}]}";
-
-else
-    
-    keys=$(security find-generic-password -a $(id -un) -c 'kpas' -C 'kpas' -s "AlfredWorkflow" -w "${keychain}" |\
-           keepassxc-cli ls -R -q -f "$database" | grep -Ev '(/|\[empty\]?)$')
-
-    if [ $? -eq 0 ]; then 
-        echo -n '{"items": ['
-        first=
-        for entry in ${keys[@]}; do
-            if [ $first ] ; then echo -n ","; fi
-            echo -n "{\"uid\":\"${entry}\",\"title\":\"${entry}\",\"arg\":\"${entry}\",\"autocomplete\":\"${entry}\",\"icon\":{\"type\":\"png\",\"path\":\"${PWD}/icon.png\"}}";
-            first=false
-        done
-        echo ']}'
-
-    else
-        echo -n "{\"items\": [{\"title\":\"Error listing database, please check config\"}]}"
-    fi
-
 fi
+
+function get_keys(){
+    local keys
+    keys=$(security find-generic-password -a $(id -un) -c 'kpas' -C 'kpas' -s "AlfredWorkflow" -w "${keychain}" |\
+                 keepassxc-cli locate -q "$database" "{query}" | grep -Ev '(/|\[empty\]?)$')
+    echo "${keys}"
+}
+
+keys=$(get_keys)
+if [ $? -ne 0 ]; then
+    echo -n "{\"items\": [{\"title\":\"Error listing database, please check config\"}]}";
+    exit
+fi
+
+echo "${keys}" | while read entry
+do
+    jq -n --arg uid "${entry}" --arg title "${entry##*/}" --arg subtitle "${entry}" '{"uid": $uid, "title": $title, "subtitle": $subtitle }'
+done | jq -c -s '{"items": .}'
