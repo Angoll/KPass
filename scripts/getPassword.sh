9b4e44a8c7b5a0d1564e830ffbefe6287131f3eb
Andreu Gonzalez <andreuglezllinas@gmail.com>
2022-03-18 23:58:45 +0100

    cache and other fixes

--- scripts/getPassword.sh
+++ scripts/getPassword.sh
@@ -1,19 +1,24 @@
-export PATH='/usr/local/bin/:/usr/bin:/Applications/KeePassXC.app/Contents/MacOS/:${PATH}'
+export PATH='/bin:/usr/local/bin/:/usr/bin:/Applications/KeePassXC.app/Contents/MacOS/:/opt/homebrew/bin:${PATH}'
 
-keePassKeyFile=""
-if [[ ! -z ${keyfile} ]]; then
-    keePassKeyFile="--key-file \"${keyfile}\""
-fi
-
-function get_keys() {
-    security find-generic-password -a $(id -un) -c 'kpas' -C 'kpas' -s "${keychainItem}" -w "${keychain}" |\
-           keepassxc-cli show -q ${keePassKeyFile} -a Password "$database" "$1"
+function get_keys {
+    if [ ! -z "${keePassKeyFile}" ]; then
+        security find-generic-password -a $(id -un) -c 'kpas' -C 'kpas' -s "${keychainItem}" -w "${keychain}" |\
+            keepassxc-cli show --key-file "${keePassKeyFile}" -q -a Password "${database}" "$1"
+    else
+        security find-generic-password -a $(id -un) -c 'kpas' -C 'kpas' -s "${keychainItem}" -w "${keychain}" |\
+            keepassxc-cli show -q -a Password "${database}" "$1"
+    fi
 }
 
 function get_errorInfo {
     exec 3<&1
-    security find-generic-password -a $(id -un) -c 'kpas' -C 'kpas' -s "${keychainItem}" -w "${keychain}" 2>&3 |\
-           keepassxc-cli ls -R ${keePassKeyFile} -f "$database" 2>&3 | grep -Ev '(/|\[empty\]?)$'
+    if [ ! -z "${keePassKeyFile}" ]; then
+        security find-generic-password -a $(id -un) -c 'kpas' -C 'kpas' -s "${keychainItem}" -w "${keychain}" 2>&3 |\
+           keepassxc-cli ls -R --key-file "${keePassKeyFile}" -f "$database" 2>&3 | grep -Ev '(/|\[empty\]?)$'
+    else
+        security find-generic-password -a $(id -un) -c 'kpas' -C 'kpas' -s "${keychainItem}" -w "${keychain}" 2>&3 |\
+           keepassxc-cli ls -R -f "$database" 2>&3 | grep -Ev '(/|\[empty\]?)$'
+    fi
     exec 3>&-
 }
 
--- scripts/listDatabase.sh
+++ scripts/listDatabase.sh
@@ -1,49 +1,56 @@
-export PATH=/usr/local/bin/:/usr/bin:/Applications/KeePassXC.app/Contents/MacOS/:/opt/homebrew/bin/:$PATH
+export PATH='/bin:/usr/local/bin/:/usr/bin:/Applications/KeePassXC.app/Contents/MacOS/:/opt/homebrew/bin:${PATH}'
 
+function get_db_keys {
+    if [ ! -z "${keePassKeyFile}" ]; then
+        security find-generic-password -a $(id -un) -c 'kpas' -C 'kpas' -s "${keychainItem}" -w "${keychain}" |\
+            keepassxc-cli locate --key-file "${keePassKeyFile}" "${database}" / -q
+    else
+        security find-generic-password -a $(id -un) -c 'kpas' -C 'kpas' -s "${keychainItem}" -w "${keychain}" |\
+            keepassxc-cli locate "${database}" / -q
+    fi
+}
 
-useKeePassKeyFile=""
-if [[ ! -z ${keePassKeyFile} ]]; then
-    useKeePassKeyFile="--key-file ${keePassKeyFile}"
-fi
-
-cacheDir=$(dirname ${keychain})
-cacheFile="$cacheDir/kpass_db.cache"
-if [ ! -f "$cacheFile" ];then
-	lastModifiedTime=0
-else
-	lastModifiedTime=$(/opt/homebrew/opt/coreutils/libexec/gnubin/stat -c %Y $cacheFile)
-fi
-currTime=$(date +%s)
-interval=$(expr $currTime - $lastModifiedTime)
-
-function get_keys() {
-    security find-generic-password -a $(id -un) -c 'kpas' -C 'kpas' -s "${keychainItem}" -w "${keychain}" |keepassxc-cli locate ${useKeePassKeyFile} "$database" / -q 
-} 
+function get_keys {
+    if [ -z "${cacheFile}" ]; then
+        get_db_keys
+    else
+        # Cache is enabled
+        if [ -f "${cacheFile}" ]; then
+            lastModifiedTime=$(GetFileInfo -d "${cacheFile}")
+        else
+            lastModifiedTime=$(date +"%m/%d/%Y %H:%M:%S")
+        fi
+        lastModifiedTime=$(date -jf "%m/%d/%Y %H:%M:%S" "${lastModifiedTime}" +%s)
+        currTime=$(date +%s)
+        interval=$( expr $currTime - $lastModifiedTime )
+        if [ ! -f "${cacheFile}" ] || [ "${interval}" -gt "${cacheTimeout}" ]; then
+            # Update cache
+            echo "$(get_db_keys)" > "${cacheFile}"
+        fi
+        # Get keys from the cache
+        cat "${cacheFile}" | grep -i "${query}"
+    fi
+}
 
 function get_errorInfo {
     exec 3<&1
     security find-generic-password -a $(id -un) -c 'kpas' -C 'kpas' -s "${keychainItem}" -w "${keychain}" 2>&3 |\
-           keepassxc-cli locate ${useKeePassKeyFile} "$database" / -q 2>&3
+        keepassxc-cli locate ${useKeePassKeyFile} '$database' / -q 2>&3
     exec 3>&-
 }
 
-if [[ -z ${database} ]] || [[ -z ${keychain} ]];
+if [[ -z "${database}" ]] || [[ -z "${keychain}" ]];
 then
     echo "{\"items\": [{\"title\":\"Not configured, please run: kpassinit\"}]}";
     exit
 fi
 
-if [ ! -f "$cacheFile" ] || [ $interval -gt 60 ];then
-	# update cache file
-	echo "$(get_keys)" > "$cacheFile"
-fi
-# get keys from cache 
-keys=($(cat $cacheFile | grep -i "{query}" ))
+keys=($(get_keys))
 if [ $? -ne 0 ]; then
     info=$(get_errorInfo | sed 's/"/\\"/g')
     info=${info//$'\n'/}
     echo "{\"items\": [{\"title\":\"Error listing database, please check config: Error: ${info}\"}]}";
     exit
 else
-    echo ${keys[@]} | sed 's/ \//\n\//g' | awk -v iconPath="${PWD}/icon.png" '{printf "{\"uid\":\"%s\", \"title\":\"%s\", \"subtitle\":\"%s\", \"arg\":\"%s\", \"autocomplete\": \"%s\", \"icon\":{\"type\":\"png\", \"path\": \"%s\"}}", $0, $0, $0, $0, $0, iconPath}' | jq -c -s '{"items": .}'
+    echo ${keys[@]} | sed 's/ \//\n\//g' | awk -v iconPath="${PWD}/icon.png" '{printf "{\"uid\":\"%s\", \"title\":\"%s\", \"subtitle\":\"%s\", \"arg\":\"%s\", \"autocomplete\": \"%s\", \"icon\":{\"type\":\"png\", \"path\": \"%s\"}}", $0, substr($0,2, length($0)), $0, $0, $0, iconPath}' | jq -c -s '{"items": .}'
 fi
